e94e02bbee2bad690081bfe5a59f9febf36d29bb
Matsubayashi <s133075@matsu.shimane-uac.jp>
2015-08-26 02:39:06 +0000

    パラメータの使用2

---
+++ main4.rb
@@ -0,0 +1,15 @@
+#04URLからのパラメータを使おう1
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+=begin
+get '/from/*/to/*' do |f,t|
+    "from #{f} to #{t}"
+end
+=end
+
+#正規表現の使用
+get %r{/users/([0-9]*)} do |i|
+    "user id=#{i}"
+end
