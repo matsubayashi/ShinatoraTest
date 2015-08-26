58aa8b2ee6548beee56682f86b5c54315d518534
Matsubayashi <s133075@matsu.shimane-uac.jp>
2015-08-26 02:03:04 +0000

    パラメータの使用1

---
+++ main3.rb
@@ -0,0 +1,18 @@
+#04URLからのパラメータを使おう1
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+=begin
+get '/hello/:name'do
+    "hello #{params[:name]}"
+end
+
+get '/hello/:name'do |n|
+    "hello #{n}"
+end
+=end
+
+get '/hello/:fname/?:lname?' do |f,l|
+    "hello #{f} #{l}"
+end
